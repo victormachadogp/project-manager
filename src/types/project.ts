fd1801d4c2cee89a339a20f2770f801206a690b6
Victor Machado <victormachadogp@gmail.com>
2025-01-15 17:51:11 -0300

    feat: project type added

---
+++ src/types/project.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  id: number
+  name: string
+  client: string
+  startDate: string
+  endDate: string
+  coverImage: string
+  isFavorite: boolean
+  createdAt: string
+}
