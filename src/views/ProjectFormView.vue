41cfb87db801291ade252e3bcddc183bff16b1f6
Victor Machado <victormachadogp@gmail.com>
2025-01-16 18:40:55 -0300

    style: Container max size in ProjectFormView

--- src/views/ProjectFormView.vue
+++ src/views/ProjectFormView.vue
@@ -1,54 +1,56 @@
 <template>
-  <ProjectFormHeader :is-editing="isEditing" />
+  <div class="max-w-[1860px] mx-auto my-10">
+    <ProjectFormHeader :is-editing="isEditing" />
 
-  <form @submit.prevent="handleSubmit" class="border border-[#DCDCDC] p-10 mx-5">
-    <div class="max-w-2xl mx-auto flex flex-col justify-center">
-      <div class="mb-6">
-        <label for="name" class="block mb-2 text-sm font-medium text-[#695CCD]">
-          Nome do projeto
-          <span class="text-xs text-[#717171] font-light">(Obrigatório)</span>
-        </label>
-        <input v-model="form.name" type="text" id="name" required
-          class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
-      </div>
-
-      <div class="mb-6">
-        <label for="client" class="block mb-2 text-sm font-medium text-[#695CCD]">
-          Cliente
-          <span class="text-xs text-[#717171] font-light">(Obrigatório)</span>
-        </label>
-        <input v-model="form.client" type="text" id="client" required
-          class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
-      </div>
-
-      <div class="grid gap-6 mb-6 md:grid-cols-2">
-        <div>
-          <label for="startDate" class="block mb-2 text-sm font-medium text-[#695CCD]">
-            Data de Início
+    <form @submit.prevent="handleSubmit" class="border border-[#DCDCDC] p-10 mx-5 rounded">
+      <div class="max-w-2xl mx-auto flex flex-col justify-center">
+        <div class="mb-6">
+          <label for="name" class="block mb-2 text-sm font-medium text-[#695CCD]">
+            Nome do projeto
             <span class="text-xs text-[#717171] font-light">(Obrigatório)</span>
           </label>
-          <input v-model="form.startDate" type="date" id="startDate" required
+          <input v-model="form.name" type="text" id="name" required
             class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
         </div>
-        <div>
-          <label for="endDate" class="block mb-2 text-sm font-medium text-[#695CCD]">
-            Data Final
+
+        <div class="mb-6">
+          <label for="client" class="block mb-2 text-sm font-medium text-[#695CCD]">
+            Cliente
             <span class="text-xs text-[#717171] font-light">(Obrigatório)</span>
           </label>
-          <input v-model="form.endDate" type="date" id="endDate" required
+          <input v-model="form.client" type="text" id="client" required
             class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
         </div>
+
+        <div class="grid gap-6 mb-6 md:grid-cols-2">
+          <div>
+            <label for="startDate" class="block mb-2 text-sm font-medium text-[#695CCD]">
+              Data de Início
+              <span class="text-xs text-[#717171] font-light">(Obrigatório)</span>
+            </label>
+            <input v-model="form.startDate" type="date" id="startDate" required
+              class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
+          </div>
+          <div>
+            <label for="endDate" class="block mb-2 text-sm font-medium text-[#695CCD]">
+              Data Final
+              <span class="text-xs text-[#717171] font-light">(Obrigatório)</span>
+            </label>
+            <input v-model="form.endDate" type="date" id="endDate" required
+              class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
+          </div>
+        </div>
+
+        <ProjectImageUpload :image-preview="imagePreview" @upload="(event) => handleImageUpload(event)"
+          @remove="removeImage" />
+
+        <button :disabled="!isFormValid || loading" class="text-white py-2 rounded-full mt-10"
+          :class="[isFormValid ? 'bg-[#695CCD] hover:bg-[#5648B0]' : 'bg-[#B2A8FF]']" type="submit">
+          {{ loading ? 'Salvando...' : 'Salvar projeto' }}
+        </button>
       </div>
-
-      <ProjectImageUpload :image-preview="imagePreview" @upload="(event) => handleImageUpload(event)"
-        @remove="removeImage" />
-
-      <button :disabled="!isFormValid || loading" class="text-white py-2 rounded-full mt-10"
-        :class="[isFormValid ? 'bg-[#695CCD] hover:bg-[#5648B0]' : 'bg-[#B2A8FF]']" type="submit">
-        {{ loading ? 'Salvando...' : 'Salvar projeto' }}
-      </button>
-    </div>
-  </form>
+    </form>
+  </div>
 </template>
 
 <script setup lang="ts">
