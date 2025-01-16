42fc035b06aa28dc57eb14b986b3d377c0be7eb6
Victor Machado <victormachadogp@gmail.com>
2025-01-16 14:15:05 -0300

    style: Add trash icon to modal

--- src/components/ModalBase.vue
+++ src/components/ModalBase.vue
@@ -2,8 +2,12 @@
     <div id="default-modal" tabindex="-1" aria-hidden="true"
         class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-50 flex justify-center">
         <div class="relative p-4 w-full max-w-lg max-h-full">
-            <div class="relative bg-white rounded-lg shadow">
-                <div class="flex items-center justify-center p-4 md:p-5 border-b rounded-t">
+            <div class="relative bg-white rounded-lg shadow flex flex-col items-center">
+                <div
+                    class="bg-[#695CCD] w-16 h-16 rounded-full flex justify-center items-center shadow-lg absolute top-[-30px]">
+                    <IconTrash />
+                </div>
+                <div class="flex items-center justify-center p-4 md:p-5 border-b rounded-t w-full mt-7">
                     <h3 class="text-xl font-semibold text-[#1F1283]">
                         Remover projeto
                     </h3>
@@ -14,7 +18,7 @@
                     </p>
                     <h3 class="text-2xl text-[#1C1930] text-center ">{{ project?.name }}</h3>
                 </div>
-                <div class="flex items-center p-4 md:p-5  rounded-b gap-4">
+                <div class="flex items-center p-4 md:p-5  rounded-b gap-4 w-full">
                     <button @click="$emit('close')" type="button"
                         class="py-2.5 px-5 ms-3 text-sm font-medium text-[#695CCD] w-full focus:outline-none bg-white rounded-full border border-[#695CCD]  focus:z-10 focus:ring-4 focus:ring-gray-100">Cancelar</button>
                     <button @click="$emit('confirm')" type="button"
@@ -28,6 +32,7 @@
 
 <script setup lang="ts">
 import type { Project } from '../types/Project';
+import IconTrash from './icons/IconTrash.vue';
 
 defineProps<{
     project: Project | null;
