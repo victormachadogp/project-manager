a9fd0251d44b552373636220b56ea6579711f36d
Victor Machado <victormachadogp@gmail.com>
2025-01-16 01:53:03 -0300

    feat: add header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div v-if="showSearchBar" class="bg-[#1C1930] flex justify-between items-center w-full">
+            <div class="opacity-0"></div>
+            <img class="w-36" src="../assets/logo.png" alt="">
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M9 17C13.4183 17 17 13.4183 17 9C17 4.58172 13.4183 1 9 1C4.58172 1 1 4.58172 1 9C1 13.4183 4.58172 17 9 17Z"
+                    stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M19 18.9999L14.65 14.6499" stroke="white" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round" />
+            </svg>
+
+
+        </div>
+        <div class="border border-[#695CCD] bg-white flex items-center gap-3 py-1 rounded">
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M8.99176 17C13.4055 17 16.9835 13.4183 16.9835 9C16.9835 4.58172 13.4055 1 8.99176 1C4.57803 1 1 4.58172 1 9C1 13.4183 4.57803 17 8.99176 17Z"
+                    fill="white" stroke="#695CCD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M18.9814 18.9999L14.6359 14.6499" stroke="#695CCD" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round" />
+            </svg>
+
+            <input class="w-full py-2" placeholder="Digite o nome do projeto..." type="text">
+        </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const showSearchBar = ref(false);
+
+
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,53 +1,56 @@
 <template>
-  <main class="bg-white m-5 flex justify-center h-screen">
-    <div v-if="projects.length === 0" class="flex items-center justify-center flex-col space-y-5 rounded h-screen">
-      <h3 class="text-[#1F1283] font-semibold text-2xl">Nenhum Projeto</h3>
-      <span class="text-[#717171]">Clique no botão abaixo para criar o primeiro e gerenciá-lo.</span>
+  <div>
+    <TheHeader />
+    <main class="bg-white m-5 flex justify-center h-screen">
+      <div v-if="projects.length === 0" class="flex items-center justify-center flex-col space-y-5 rounded h-screen">
+        <h3 class="text-[#1F1283] font-semibold text-2xl">Nenhum Projeto</h3>
+        <span class="text-[#717171]">Clique no botão abaixo para criar o primeiro e gerenciá-lo.</span>
 
-      <RouterLink to="/project" class="bg-[#695CCD] text-white px-5 py-3 rounded-full">
-        Novo Projeto
-      </RouterLink>
-    </div>
+        <RouterLink to="/project" class="bg-[#695CCD] text-white px-5 py-3 rounded-full">
+          Novo Projeto
+        </RouterLink>
+      </div>
 
-    <div v-else>
-      <div class="flex justify-between items-center">
-        <div class="flex items-center">
-          <h3 class="text-2xl">Projetos</h3>
-          <span class="text-sm">({{ filteredProjects.length }})</span>
+      <div v-else>
+        <div class="flex justify-between items-center">
+          <div class="flex items-center">
+            <h3 class="text-2xl">Projetos</h3>
+            <span class="text-sm">({{ filteredProjects.length }})</span>
+          </div>
+
+
+
+          <div class="flex items-center gap-4">
+            <label class="inline-flex items-center cursor-pointer">
+              <input type="checkbox" v-model="showOnlyFavorites" class="sr-only peer">
+              <div
+                class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-[#695CCD] rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#695CCD]">
+              </div>
+              <span class="ms-3 text-sm font-medium text-[#717171]">Apenas favoritos</span>
+            </label>
+
+            <select v-model="sortBy" class="border border-[#DCDCDC] rounded-lg px-4 py-2 text-sm text-[#717171]">
+              <option value="alphabetical">Ordem alfabética</option>
+              <option value="startDate">Iniciados mais recentes</option>
+              <option value="endDate">Prazo mais próximo</option>
+            </select>
+
+            <RouterLink to="/project" class="bg-[#695CCD] text-white p-5 py-3 rounded-full">
+              Novo Projeto
+            </RouterLink>
+          </div>
         </div>
 
-
-
-        <div class="flex items-center gap-4">
-          <label class="inline-flex items-center cursor-pointer">
-            <input type="checkbox" v-model="showOnlyFavorites" class="sr-only peer">
-            <div
-              class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-[#695CCD] rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#695CCD]">
-            </div>
-            <span class="ms-3 text-sm font-medium text-[#717171]">Apenas favoritos</span>
-          </label>
-
-          <select v-model="sortBy" class="border border-[#DCDCDC] rounded-lg px-4 py-2 text-sm text-[#717171]">
-            <option value="alphabetical">Ordem alfabética</option>
-            <option value="startDate">Iniciados mais recentes</option>
-            <option value="endDate">Prazo mais próximo</option>
-          </select>
-
-          <RouterLink to="/project" class="bg-[#695CCD] text-white p-5 py-3 rounded-full">
-            Novo Projeto
-          </RouterLink>
+        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 mt-5">
+          <ProjectCard v-for="project in filteredProjects" :key="project.id" :project="project"
+            @delete="showDeleteModal = true; projectToDelete = project" />
         </div>
       </div>
 
-      <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 mt-5">
-        <ProjectCard v-for="project in filteredProjects" :key="project.id" :project="project"
-          @delete="showDeleteModal = true; projectToDelete = project" />
-      </div>
-    </div>
-
-    <ModalBase v-if="showDeleteModal" :project="projectToDelete" @confirm="handleDeleteProject"
-      @close="showDeleteModal = false" />
-  </main>
+      <ModalBase v-if="showDeleteModal" :project="projectToDelete" @confirm="handleDeleteProject"
+        @close="showDeleteModal = false" />
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -56,6 +59,7 @@ import ProjectCard from '@/components/ProjectCard.vue';
 import { useProjectStore } from '../stores/projectStore';
 import ModalBase from '../components/ModalBase.vue';
 import type { Project, ProjectSortOption } from '../types/Project';
+import TheHeader from '../components/TheHeader.vue';
 
 const store = useProjectStore();
 const showDeleteModal = ref(false);
