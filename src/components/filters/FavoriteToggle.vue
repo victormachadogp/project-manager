932bdf186639d0cebc29f62837b61cf59a417ae8
Victor Machado <victormachadogp@gmail.com>
2025-01-16 19:50:38 -0300

    style: Toggle color changed

--- src/components/filters/FavoriteToggle.vue
+++ src/components/filters/FavoriteToggle.vue
@@ -3,7 +3,7 @@
         <input type="checkbox" :checked="modelValue" @change="$emit('update:modelValue', $event.target.checked)"
             class="sr-only peer">
         <div
-            class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-[#695CCD] rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#695CCD]">
+            class="relative w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-2 peer-focus:ring-yellow-300  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all  peer-checked:bg-yellow-400">
         </div>
         <span class="ms-3 text-sm font-medium text-[#717171]">Apenas favoritos</span>
     </label>
