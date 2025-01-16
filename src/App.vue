6d196ab6453806faa313b994d8dbb6659b6fd28a
Victor Machado <victormachadogp@gmail.com>
2025-01-16 17:53:34 -0300

    style: Remove unused href code

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
 
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
+
     </div>
   </header>
 
