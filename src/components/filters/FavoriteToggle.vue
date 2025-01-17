ec9c3fa27ae10588de7221400ef9d9e66a952735
Victor Machado <victormachadogp@gmail.com>
2025-01-17 01:21:05 -0300

    refactor: Toggle TS

--- src/components/filters/FavoriteToggle.vue
+++ src/components/filters/FavoriteToggle.vue
@@ -1,9 +1,8 @@
 <template>
     <label class="inline-flex items-center cursor-pointer">
-        <input type="checkbox" :checked="modelValue" @change="$emit('update:modelValue', $event.target.checked)"
-            class="sr-only peer">
+        <input type="checkbox" :checked="modelValue" @change="onChange" class="sr-only peer" />
         <div
-            class="relative w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-2 peer-focus:ring-yellow-300  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all  peer-checked:bg-yellow-400">
+            class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-2 peer-focus:ring-yellow-300 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-yellow-400">
         </div>
         <span class="ms-3 text-sm font-medium text-[#717171]">Apenas favoritos</span>
     </label>
@@ -11,10 +10,17 @@
 
 <script setup lang="ts">
 defineProps<{
-    modelValue: boolean
+    modelValue: boolean;
 }>();
 
-defineEmits<{
-    (e: 'update:modelValue', value: boolean): void
+const emit = defineEmits<{
+    (e: 'update:modelValue', value: boolean): void;
 }>();
+
+const onChange = (event: Event) => {
+    const input = event.target as HTMLInputElement | null;
+    if (input) {
+        emit('update:modelValue', input.checked);
+    }
+};
 </script>
